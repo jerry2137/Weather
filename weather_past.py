e53cdb5f791361bcd4a97285436a7302f953f81f
jerry2137 <77885705+jerry2137@users.noreply.github.com>
2022-10-11 16:12:52 +0800

    Update weather_past.py

--- weather_past.py
+++ weather_past.py
@@ -12,7 +12,7 @@ import weather_missing
 def past():
     #call weather_ilens to download the missing data from ilens
     time_period = date_range(start = '2016-12-08', end = datetime.today() - timedelta(days = 1), freq = '1D')
-    #weather_ilens.ilens(time_period, past=True)
+    weather_ilens.ilens(time_period, past=True)
 
     #format the file to suit the other files from HKO
     weather_format.format(past=True)
