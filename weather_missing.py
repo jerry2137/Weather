1c88f4f63c19bda4165eff69c46ff2a7dd312370
jerry2137 <77885705+jerry2137@users.noreply.github.com>
2022-10-11 16:09:03 +0800

    Update weather_missing.py

--- weather_missing.py
+++ weather_missing.py
@@ -8,128 +8,15 @@ from collections import OrderedDict
 import shutil
 
 #-------------------------------------------------------------------------------------------
-#import weather_ilens
-#import weather_format
-#call other script
-import importlib.util
-import sys
-
-spec = importlib.util.spec_from_file_location("format", "D:/test_development/Weather/Weather_format/weather_format.py")
-weather_format = importlib.util.module_from_spec(spec)
-sys.modules["format"] = weather_format
-spec.loader.exec_module(weather_format)
-
-spec = importlib.util.spec_from_file_location("ilens", "D:/test_development/Weather/Weather_down/weather_ilens.py")
-weather_ilens = importlib.util.module_from_spec(spec)
-sys.modules["ilens"] = weather_ilens
-spec.loader.exec_module(weather_ilens)
+import weather_ilens
+import weather_format
 #-------------------------------------------------------------------------------------------
-
-
-#get all missing time(not necessary for now)
-def all_missing(dates):
-    #-------------------------------------------------------------------------------------------
-    data_file_path = "D:/test_development/Weather/Weather_update/data/"
-    #-------------------------------------------------------------------------------------------
-
-    all_missing ={}
-
-    for date in dates:
-        date_str = date.strftime('%Y%m%d')
-
-        if not os.path.isdir(data_file_path + date_str):
-            os.mkdir(data_file_path + date_str)
-
-        time_period = date_range(start = date, end = date + timedelta(days=1), freq = '10t')
-
-        file_path = data_file_path + date_str
-
-        missing = {}
-
-        for file_name in os.listdir(file_path):
-            #create an ordered dictionary to store the missing time
-            missing_ordereddict = OrderedDict()
-
-            #put the current data in to data_list
-            with open(file_path + file_name, 'r') as date_file:
-                date_file = csv.reader(date_file)
-
-                data_list = [row for row in date_file]
-
-
-            data_limit = len(data_list)
-            data_count = 0
-
-            time_limit = len(time_period)
-            time_count = 0
-
-            #compare the donwloaded data and the required time interval
-            while True:
-
-                #end of the data
-                if data_count == data_limit:
-                    for i in range(time_count, time_limit):
-                        missing_ordereddict[time_period[i]] = True
-                    break
-
-                #end of the time interval
-                if time_count == time_limit:
-                    break
-
-                if len(data_list[data_count]) == 0:
-                    continue
-                
-                #load data
-                data = datetime.strptime(data_list[data_count][0], '%Y/%m/%d %H:%M')
-                time = time_period[time_count]
-
-                time_count += 1
-                
-                #check if the data and the required time is closer than 10 mins
-                if data - time < timedelta(minutes=10):
-                    if (data_list[data_count][1] != '') & (data_list[data_count][2] != ''):
-                        missing_ordereddict[time] = False
-                    else:
-                        missing_ordereddict[time] = True
-                    data_count += 1
-                    
-                else:
-                    missing_ordereddict[time] = True
-
-            #put all the missing time into the missing dictionary as a list with key of the site
-            missing[file_name] = missing_ordereddict
-
-        all_missing[date] = missing
-
-    return all_missing
-
-
-#print the missing interval and check manually(not necessary for now)
-def check_boundaries(missing):
-    
-    for site, data in missing.items():
-        print(site)
-
-        boundaries = []
-        begin_missing = False
-
-        for time, flag in data.items():
-            if flag != begin_missing:
-                boundaries.append(time)
-                begin_missing = flag
-
-        last_data = data.popitem(last=True)
-        if last_data[0] != boundaries[-1]:
-            boundaries.append(last_data[0])
-
-        print(boundaries)
 
 
 #check if there's any data lackage
 def check(dates):
     #-------------------------------------------------------------------------------------------
-    #data_file_path = "data/"
-    data_file_path = "D:/test_development/Weather/Weather_update/data/"
+    data_file_path = "data/"
     #-------------------------------------------------------------------------------------------
 
     missing_date = []
@@ -189,10 +76,8 @@ def check(dates):
 def combine():
 
     #-------------------------------------------------------------------------------------------
-    #data_file_path = 'data/'
-    #missing_file_path = 'missing_data/'
-    data_file_path = "D:/test_development/Weather/Weather_update/data/"
-    missing_file_path = "D:/test_development/Weather/Weather_update/missing_data/"
+    data_file_path = 'data/'
+    missing_file_path = 'missing_data/'
     #-------------------------------------------------------------------------------------------
 
     #iterate through all the dates
@@ -254,8 +139,7 @@ def fill(start_date, end_date):
 
     #-------------------------------------------------------------------------------------------
     #delete the zip files
-    #zip_file_path = 'zip_file/'
-    zip_file_path = 'D:/test_development/Weather/Weather_down/zip_file/'
+    zip_file_path = 'zip_file/'
     for date_zip_file_name in os.listdir(zip_file_path):
         os.remove(zip_file_path + date_zip_file_name)
     #-------------------------------------------------------------------------------------------
