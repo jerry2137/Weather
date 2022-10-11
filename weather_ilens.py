436179f83ddd149662942b0281b7bf9428f0e344
jerry2137 <77885705+jerry2137@users.noreply.github.com>
2022-10-11 16:09:44 +0800

    Update weather_ilens.py

--- weather_ilens.py
+++ weather_ilens.py
@@ -1,20 +1,19 @@
 import requests
 import os
 from datetime import datetime
+from tqdm import tqdm
 
 
 #download the zipped weather data of the required dates from ilens
 def ilens(dates, past=False):
 
-    for date in dates:
+    for date in tqdm(dates):
         url = r'https://i-lens.hk/hkweather/file_download.php?file_type=history_minute_record&file={date}'.format(date = date.strftime('%Y-%m-%d'))
         zip_file = requests.get(url)
         #-------------------------------------------------------------------------------------------
-        #save_path = 'zip_file/'
-        save_path = r'D:\test_development\Weather\Weather_down\zip_file/'
+        save_path = 'zip_file/'
         if past:
-            #save_path = 'past_zip_file/'
-            save_path = r'D:\test_development\Weather\Weather_down\past_zip_file/'
+            save_path = 'past_zip_file/'
         #-------------------------------------------------------------------------------------------
         file_name = url.split("=")[2] + '.zip'
         file_save_path = os.path.join(save_path, file_name)
